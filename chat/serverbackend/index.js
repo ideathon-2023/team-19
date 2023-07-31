d82261c5454dbbf273b257ca3d66102f0c8fbe41
Abhishek Sood <nyp.abhishek@gmail.com>
2023-07-31 10:29:34 +0530

    update

--- chat/serverbackend/index.js
+++ chat/serverbackend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://127.0.0.1:27017", {
   dbName: "GhostTalk",
